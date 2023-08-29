1c0942cb1fbfe8d8f43e1a4b134b10437f46e69e
Carlos Venegas <cvenegasa@udec.cl>
2023-08-29 16:00:50 -0400

    Added invalid hour test for greeter

--- tests/test_greeter.py
+++ tests/test_greeter.py
@@ -10,3 +10,7 @@ def test_get_greeting(current_hour, expected_greeting):
     ohce_instance = Ohce("Pedro")
     greeting = ohce_instance.get_greeting(current_hour)
     assert greeting == expected_greeting
+def test_get_greeting_invalid_hour():
+    ohce_instance = Ohce("Pedro")  #
+    with pytest.raises(ValueError):
+        ohce_instance.get_greeting(-1)  
