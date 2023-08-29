021b6f401ed43fb0f6523cd516987b79aa618fad
Carlos Venegas <cvenegasa@udec.cl>
2023-08-29 16:39:34 -0400

    test_stop tweaked

--- tests/test_stop.py
+++ tests/test_stop.py
@@ -1,13 +1,14 @@
 import pytest
+from app.ohce_kata import Ohce
+
 def test_stop_command(capfd):
     ohce_instance = Ohce("Pedro") 
 
     input = "Stop!"
 
-    # Capture the output using the capfd fixture
-    with pytest.raises(SystemExit):
-        ohce_instance.ohce(input_func=input, output_func=print)
 
-    # Check if the "Adios" message is in the captured output
+    with pytest.raises(SystemExit):
+        ohce_instance.ohce(input=input, output_f=print)
+
     captured = capfd.readouterr()
     assert "Adios Pedro" in captured.out
