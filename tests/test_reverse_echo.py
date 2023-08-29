189101a8fab81c6579aa2f647eea8df913357de7
Carlos Venegas <cvenegasa@udec.cl>
2023-08-29 16:55:08 -0400

    Added Stop! message to test_reverse_echo

--- tests/test_reverse_echo.py
+++ tests/test_reverse_echo.py
@@ -1,13 +1,15 @@
+import sys
 import pytest
 from app.ohce_kata import Ohce
 
-def test_reverse_echo():
+def test_reverse_echo(capfd):
     ohce_instance= Ohce("Pedro")
-
-    input_word = "hola"
+    input_sequence=["hola","Stop!"]
     expected_output = "aloh"
+    def input_func(prompt):
+        return input_sequence.pop(0)
     with pytest.raises(SystemExit):
-        ohce_instance.ohce(input_f= lambda _: input_word, output_f=print)
+        ohce_instance.ohce(input= input_func)
     captured= capfd.readouterr()
 
     assert expected_output in captured.out
