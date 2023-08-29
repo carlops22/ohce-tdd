a1f11ab4457aa571f7d9e387d47561244547e975
Carlos Venegas <cvenegasa@udec.cl>
2023-08-29 12:03:51 -0400

    source VENV/bin/activate

---
+++ tests/test_acceptance.py
@@ -0,0 +1,21 @@
+def test_ohce_acceptance():
+    input_stream = ["Pedro", "hola", "oto", "stop"]
+    output_stream = []
+
+    def mock_input(prompt):
+        return input_stream.pop(0)
+
+    def mock_output(message):
+        output_stream.append(message)
+
+    ohce(mock_input, mock_output)
+
+    expected_output = [
+        "¡Buenos días Pedro!",
+        "aloh",
+        "oto",
+        "¡Bonita palabra!",
+        "pots",
+        "Adios Pedro"
+    ]
+    assert output_stream == expected_output
