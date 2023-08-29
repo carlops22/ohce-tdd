d96afff481516edc90b1a3952e69baa03ffba007
Carlos Venegas <cvenegasa@udec.cl>
2023-08-29 16:39:50 -0400

    adding code to pass stop test (not yet)

--- app/ohce_kata.py
+++ app/ohce_kata.py
@@ -1,5 +1,5 @@
 import datetime
-
+import sys
 class Ohce:
     def __init__(self,name):
         self.name = name
@@ -8,10 +8,10 @@ class Ohce:
         now = datetime.datetime.now()
         current_hour = now.hour
         return current_hour
-
-    def get_greeting(self, current_hour=None):
+    @staticmethod   
+    def get_greeting(current_hour=None):
         if current_hour is None:
-            current_hour= self.get_current_hour()
+            current_hour= Ohce.get_current_hour()
         if not (0 <= current_hour < 24):
             raise ValueError("Invalid hour")
         if 6 <= current_hour < 12:
@@ -24,8 +24,15 @@ class Ohce:
     def return_greeting(self, current_hour= None):
         greeting = self.get_greeting(current_hour)
         return f"{greeting} {self.name}!"
-    def ohce(self, input_f, output_f):
-        output_f(self.return_greeting()) #
+    def ohce(self, input, output_f):
+        output_f(self.return_greeting()) 
+        while True:
+            user_input = input("> ")
+            if user_input == "Stop!":
+                output_f(f"Adios {self.name}")
+                break
+
+
 
         
     
