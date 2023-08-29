e41c7f30c8446e401b2a95d9230112c371e0b136
Carlos Venegas <cvenegasa@udec.cl>
2023-08-29 13:07:41 -0400

    Added minimal implementation to pass greeting test

--- app/ohce_kata.py
+++ app/ohce_kata.py
@@ -1,2 +1,22 @@
-def ohce(input, output):
-    print('not implemented')
+import datetime
+def get_current_hour():
+    now = datetime.datetime.now()
+    current_hour = now.hour
+    return current_hour
+class Ohce:
+    def __init__(self,name):
+        self.name = name
+    
+    def get_greeting(self):
+        current_hour= get_current_hour()
+        if 20 <= current_hour <6:
+            return "¡Buenas noches"
+        elif 6 <= current_hour <12:
+            return "¡Buenos días"
+        else:
+            return "¡Buenas tardes"
+
+    def return_greeting(self):
+        greeting = self.get_greeting()
+        return f"{greeting} {self.name}!"
+    
